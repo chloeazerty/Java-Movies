60b63dc45434a28e2922a38ac895f110d0112b16
chloe azerty <chloe.rotaru@yahoo.com>
2022-07-19 10:54:45 +0200

    correction

--- Movies.java
+++ Movies.java
@@ -1,20 +1,21 @@
 import java.util.Arrays;
 
 public class Movies {
-    String[] indianaMovies = {"Indiana Jones and the Kingdom of the Crystal Skull", "Indiana Jones and the Last Crusade", "Indiana Jones and the Temple of Doom"};
-
-    String[] firstMovieActors = {"Harrison Ford", "Shia LaBoeuf", "Karen Allen"};
-    String[] secondMovieActors = {"Harrison Ford", "Sean Connery", "Denholm Elliott"};
-    String[] thirdMovieActors = {"Harrison Ford", "Kate Capshaw", "Ke Huy Quan"};
-
-    String[][] actors = new String[][]
-    {
-        firstMovieActors,
-        secondMovieActors,
-        thirdMovieActors
-    };
 
     public static void main(String[] args) {
+        String[] indianaMovies = {"Indiana Jones and the Kingdom of the Crystal Skull", "Indiana Jones and the Last Crusade", "Indiana Jones and the Temple of Doom"};
+
+        String[] firstMovieActors = {"Harrison Ford", "Shia LaBoeuf", "Karen Allen"};
+        String[] secondMovieActors = {"Harrison Ford", "Sean Connery", "Denholm Elliott"};
+        String[] thirdMovieActors = {"Harrison Ford", "Kate Capshaw", "Ke Huy Quan"};
+
+        String[][] actors = new String[][]
+                {
+                        firstMovieActors,
+                        secondMovieActors,
+                        thirdMovieActors
+                };
+
         for (int i = 0; i < indianaMovies.length; i++) {
             System.out.println("Dans le film" + indianaMovies[i] + " , les principaux acteurs sont : " + actors[i][0] + "," + actors[i][1] + "," + actors[i][2]);
         }
